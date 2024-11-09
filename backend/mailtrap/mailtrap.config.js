0cf6664e188f6b053ab7ef5b2197bbe52fd04d6c
moadboujamaa <moadboujmaa@gmail.com>
2024-11-09 11:50:24 +0000

    mailtrap working

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -1,29 +1,35 @@
 import { MailtrapClient } from "mailtrap";
-import dotenv from "dotenv"
+
+import dotenv from "dotenv";
 
 dotenv.config()
 
-const TOKEN = "408ac1e13371edbc4d35e4649b1f0db5";
-
 const client = new MailtrapClient({
-  	token: TOKEN,
+	token: process.env.MAILTRAP_TOKEN,
+	endpoint: process.env.MAILTRAP_ENDPOINT
 });
 
+
 const sender = {
-	email: "hello@moadboujamaa.me",
-name: "Test User",
+
+	email: "moad@moadboujamaa.me",
+	name: "Moad Boujamaa",
+
 };
+
 const recipients = [
 	{
 		email: "moadboujmaa@gmail.com",
 	}
 ];
 
-client.send({
-	from: sender,
-	to: recipients,
-	subject: "You are awesome!",
-	text: "Congrats for sending test email with Mailtrap!",
-	category: "Integration Test",
-})
-.then(console.log, console.error);
+
+client
+	.send({
+		from: sender,
+		to: recipients,
+		subject: "You are awesome!",
+		text: "Congrats for sending test email with Mailtrap!",
+		category: "Integration Test",
+	})
+	.then(console.log, console.error);
